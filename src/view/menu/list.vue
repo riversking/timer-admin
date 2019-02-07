08cb3a0f3f2c94598a6a99cb16c9888198aea3a6
riversking <kingjames6236@163.com>
2019-02-07 20:00:28 +0800

    update

--- src/view/menu/list.vue
+++ src/view/menu/list.vue
@@ -1,10 +1,49 @@
 <template>
   <Card>
-      <Row :gutter="12">
-        <Col span="24">
-          <Table border :columns="roleColumns" :data="listData"></Table>
-        </Col>
-      </Row>
+    <Row>
+      <ButtonGroup :size="large">
+        <Button type="primary">新增</Button>
+        <Button type="primary">编辑</Button>
+        <Button type="primary">删除</Button>
+      </ButtonGroup>
+    </Row>
+    <Row>
+      <Col span="6">
+        <Tree :data="data1"></Tree>
+      </Col>
+      <Col span="18">
+        <Form :model="formItem" :label-width="80">
+          <FormItem label="父亲节点">
+            <Input v-model="formItem.input" placeholder="请输入父亲节点"></Input>
+          </FormItem>
+          <FormItem label="节点ID">
+            <Input v-model="formItem.input" placeholder="请输入节点ID"></Input>
+          </FormItem>
+          <FormItem label="标题">
+            <Input v-model="formItem.input" placeholder="请输入标题"></Input>
+          </FormItem>
+          <FormItem label="权限标识">
+            <Input v-model="formItem.input" placeholder="请输入权限标识"></Input>
+          </FormItem>
+          <FormItem label="图标">
+            <Input v-model="formItem.input" placeholder="请输入图标"></Input>
+          </FormItem>
+          <FormItem label="类型">
+            <Select v-model="formItem.select">
+              <Option value="1">按钮</Option>
+              <Option value="2">菜单</Option>
+            </Select>
+          </FormItem>
+          <FormItem label="排序">
+            <Input v-model="formItem.input" placeholder="请输入排序"></Input>
+          </FormItem>
+          <FormItem>
+            <Button type="primary">新增</Button>
+            <Button style="margin-left: 8px">取消</Button>
+          </FormItem>
+        </Form>
+      </Col>
+    </Row>
   </Card>
 </template>
 <script>
@@ -13,47 +52,45 @@
   export default {
 
     name: 'menuList',
-    components: {
-    },
+    components: {},
     data() {
       return {
-        roleColumns: [
+        formItem: {},
+        data1: [
           {
-            title: '序号',
-            key: 'id'
-          },
-          {
-            title: '角色名称',
-            key: 'roleName'
-          },
-          {
-            title: '角色标识',
-            key: 'roleCode'
-          },
-          {
-            title: '角色描述',
-            key: 'roleDesc'
-          },
-          {
-            title: '创建时间',
-            key: 'createTime'
-          },
-          {
-            title: '更新时间',
-            key: 'updateTime'
-          },
-          {
-            title: '操作',
-            key: 'action',
-            align: 'center',
-            width: 250,
+            title: 'parent 1',
+            expand: true,
+            children: [
+              {
+                title: 'parent 1-1',
+                expand: true,
+                children: [
+                  {
+                    title: 'leaf 1-1-1'
+                  },
+                  {
+                    title: 'leaf 1-1-2'
+                  }
+                ]
+              },
+              {
+                title: 'parent 1-2',
+                expand: true,
+                children: [
+                  {
+                    title: 'leaf 1-2-1'
+                  },
+                  {
+                    title: 'leaf 1-2-1'
+                  }
+                ]
+              }
+            ]
           }
         ]
       }
     },
-    methods: {
-
-    },
+    methods: {},
     computed: {
       ...mapState({
         listData: state => state.role.listData
